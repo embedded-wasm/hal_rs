b4ff26ab86969d57f217d63fc5cfef21b0b4e94d
ryan kurte <ryankurte@gmail.com>
2022-11-26 12:22:42 +1300

    move tests to src to avoid cargo wobblies

---
+++ src/tests/test_i2c.rs
@@ -0,0 +1,25 @@
+
+use wasm_embedded_hal::i2c::*;
+use embedded_hal::i2c::blocking::*;
+
+fn main() {
+    println!("Opening I2c device");
+
+    let mut s = I2c::init(0, 4_000_000, -1, -1).unwrap();
+    let addr = 0x0a;
+
+    println!("I2C Write");
+    let a = [0xaa, 0xbb, 0xcc];
+    s.write(addr, &a).unwrap();
+
+    println!("I2C Read");
+    let mut b = [0u8; 4];
+    s.read(addr, &mut b).unwrap();
+    println!("RX data: {:02x?}", b);
+    assert_eq!(&b, &[0x11, 0x22, 0x33, 0x44]);
+
+    println!("I2C WriteRead");
+    s.write_read(addr, &a, &mut b).unwrap();
+    assert_eq!(&b, &[0x22, 0x33, 0x44, 0x55]);
+
+}
